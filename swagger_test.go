b7bfeb325906f290541af2f7828f2f09b72ead3d
Drew Silcock <drew.silcock@stfc.ac.uk>
2020-01-06 16:24:12 +0000

    Expand tests to include config.URL and config.OAuth.
    Also add test for the 'index.htmlnotreally' bug.

--- swagger_test.go
+++ swagger_test.go
@@ -10,13 +10,64 @@ import (
 )
 
 func TestWrapHandler(t *testing.T) {
-
 	router := echo.New()
 
 	router.GET("/*", WrapHandler)
 
 	w1 := performRequest("GET", "/index.html", router)
 	assert.Equal(t, 200, w1.Code)
+
+	w2 := performRequest("GET", "/", router)
+	assert.Equal(t, 200, w2.Code)
+	assert.Equal(t, w1.Body.String(), w2.Body.String())
+
+	w3 := performRequest("GET", "/doc.json", router)
+	assert.Equal(t, 200, w3.Code)
+
+	w4 := performRequest("GET", "/favicon-16x16.png", router)
+	assert.Equal(t, 200, w4.Code)
+
+	w5 := performRequest("GET", "/notfound", router)
+	assert.Equal(t, 404, w5.Code)
+
+	w6 := performRequest("GET", "/index.htmlnotfound", router)
+	assert.Equal(t, 404, w6.Code)
+}
+
+func TestConfig(t *testing.T) {
+	router := echo.New()
+
+	swaggerHandler := EchoWrapHandler(func(c *Config) {
+		c.URL = "some-other-name.json"
+	})
+	router.GET("/*", swaggerHandler)
+
+	w1 := performRequest("GET", "/index.html", router)
+	assert.Equal(t, 200, w1.Code)
+
+	w2 := performRequest("GET", "/some-other-name.json", router)
+	assert.Equal(t, 200, w2.Code)
+}
+
+func TestConfigWithOAuth(t *testing.T) {
+	router := echo.New()
+
+	swaggerHandler := EchoWrapHandler(func(c *Config) {
+		c.OAuth = &OAuthConfig{
+			ClientId: "my-client-id",
+			Realm:    "my-realm",
+			AppName:  "My App Name",
+		}
+	})
+	router.GET("/*", swaggerHandler)
+
+	w1 := performRequest("GET", "/index.html", router)
+	assert.Equal(t, 200, w1.Code)
+	body1 := w1.Body.String()
+	assert.Contains(t, body1, "initOAuth({")
+	assert.Contains(t, body1, "clientId: \"my-client-id\"")
+	assert.Contains(t, body1, "realm: \"my-realm\"")
+	assert.Contains(t, body1, "appName: \"My App Name\"")
 
 	w2 := performRequest("GET", "/doc.json", router)
 	assert.Equal(t, 200, w2.Code)
@@ -26,7 +77,6 @@ func TestWrapHandler(t *testing.T) {
 
 	w4 := performRequest("GET", "/notfound", router)
 	assert.Equal(t, 404, w4.Code)
-
 }
 
 func performRequest(method, target string, e *echo.Echo) *httptest.ResponseRecorder {
